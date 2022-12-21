b74cbee9fffc9b05a90cd82ac33c4b3f3a4f4854
fmendezh <federicomh@gmail.com>
2022-12-21 11:37:34 +0100

    incrementing in one the predicate count,  distance from centroid added

--- src/test/java/org/gbif/occurrence/query/PredicateCounterTest.java
+++ src/test/java/org/gbif/occurrence/query/PredicateCounterTest.java
@@ -90,7 +90,7 @@ public class PredicateCounterTest {
     ConjunctionPredicate and = new ConjunctionPredicate(ands);
 
     int c = counter.count(and);
-    assertEquals(236, c);
+    assertEquals(237, c);
   }
 
   @Test
